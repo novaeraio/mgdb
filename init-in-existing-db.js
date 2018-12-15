7a5a4f4a9329541ca5537390e170f809278773b2
azhang <azhang@nextpathway.com>
2018-12-15 12:42:21 -0500

    init commit

--- init-in-existing-db.js
+++
@@ -1,4 +0,0 @@
-// This data will be inserted into database given by 'MONGO_INITDB_DATABASE' environment variable.
-// If the environment variable is not set then it will be inserted into database name 'test'
-
-db.people.insert({"firstname" : "kubernetes", "lastname" : "database" });
--- init.js
+++ init.js
@@ -1,2 +1,3 @@
 db = db.getSiblingDB('kubedb');
-db.people.insert({"firstname" : "kubernetes", "lastname" : "database" });
+db.billinguser.insert({"userid" : "k8sblademan1", "billingid" : "k8sbillable1" });
+db.billinguser.insert({"userid" : "k8sblademan2", "billingid" : "k8sbillable2" });
